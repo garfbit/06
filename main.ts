a8fed902e4efe6a2f45b77fd20d975c411e76a98
garfbit <104676660+garfbit@users.noreply.github.com>
2022-09-11 09:46:57 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let numero = 0
+let R = 0
+let numeroinverso = 0
 basic.forever(function () {
-	
+    numero = 12345
+    R = numero % 10
+    numero = numero / 10
+    numeroinverso = R * 10
+    R = numero % 10
+    numero = numero / 10
+    numeroinverso = (numeroinverso + R) * 10
+    R = numero % 10
+    numero = numero / 10
+    numeroinverso = (numeroinverso + R) * 10
+    R = numero % 10
+    numero = numero / 10
+    numeroinverso = (numeroinverso + R) * 10
+    numeroinverso = numeroinverso + numero
+    basic.showNumber(numeroinverso)
 })
